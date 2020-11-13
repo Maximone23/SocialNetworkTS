5c12789e2a575ca183fd754d13e73eb069266619
Maximone23 <maxim.one23@gmail.com>
2020-11-14 01:07:46 +0500

    Add redux-form. Refactoring Login.tsx

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,43 @@
 import React from 'react'
+import {Field, InjectedFormProps, reduxForm} from "redux-form"
 
-const Login = () => {
+type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
+}
+
+const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={"Login"} component={"input"} name={"login"}/>
+            </div>
+            <div>
+                <Field placeholder={"Password"} component={"input"} name={"password"}/>
+            </div>
+            <div>
+                <Field type={"checkbox"} component={"input"} name={"checkbox"}/>remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+export const LoginReduxForm = reduxForm<FormDataType>({form: 'login'})(LoginForm)
+
+export const Login = () => {
+    const onSubmit = (formData: FormDataType) => {
+        console.log(formData)
+    }
     return (
         <div>
-           <h1>Login</h1>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
         </div>
-    );
-};
+    )
+}
 
 export default Login
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -5,6 +5,7 @@ import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunk from 'redux-thunk'
+import {reducer as formReducer} from 'redux-form'
 
 
 let rootReducer = combineReducers({
@@ -12,7 +13,8 @@ let rootReducer = combineReducers({
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
     sidebar: sidebarReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 type RootReducerType = typeof rootReducer
