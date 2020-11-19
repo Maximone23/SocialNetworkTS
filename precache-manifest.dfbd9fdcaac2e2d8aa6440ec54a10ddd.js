becdfb16011b5caa85e983e6a58088f4660104d9
Maximone23 <maxim.one23@gmail.com>
2020-11-20 04:32:01 +0500

    Updates

---
+++ precache-manifest.dfbd9fdcaac2e2d8aa6440ec54a10ddd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b170446ae1edec9555ce932ec0b9359f",
+    "url": "/SocialNetworkTS/index.html"
+  },
+  {
+    "revision": "43db336bba06e1377913",
+    "url": "/SocialNetworkTS/static/css/3.c68bdf5e.chunk.css"
+  },
+  {
+    "revision": "8a9bc4e95224ca4b24ed",
+    "url": "/SocialNetworkTS/static/css/4.5d0afe76.chunk.css"
+  },
+  {
+    "revision": "29d3922b6af8b649e513",
+    "url": "/SocialNetworkTS/static/css/main.09921883.chunk.css"
+  },
+  {
+    "revision": "57e0bca543769b5cf4e7",
+    "url": "/SocialNetworkTS/static/js/2.2f33ecc3.chunk.js"
+  },
+  {
+    "revision": "eb4ce32374aede0bb82754a70a09743d",
+    "url": "/SocialNetworkTS/static/js/2.2f33ecc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43db336bba06e1377913",
+    "url": "/SocialNetworkTS/static/js/3.33eba628.chunk.js"
+  },
+  {
+    "revision": "8a9bc4e95224ca4b24ed",
+    "url": "/SocialNetworkTS/static/js/4.3b072294.chunk.js"
+  },
+  {
+    "revision": "29d3922b6af8b649e513",
+    "url": "/SocialNetworkTS/static/js/main.45708571.chunk.js"
+  },
+  {
+    "revision": "8cf0cb11c058c096fd85",
+    "url": "/SocialNetworkTS/static/js/runtime-main.0100db2c.js"
+  },
+  {
+    "revision": "259dde4cc7812cdf0a9ece3978803f9c",
+    "url": "/SocialNetworkTS/static/media/user-photo.259dde4c.png"
+  }
+]);
