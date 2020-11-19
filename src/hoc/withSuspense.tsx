357391cf17b6c8b58e0eb110a781104f964ed38d
Maximone23 <maxim.one23@gmail.com>
2020-11-20 03:53:28 +0500

    Fixing withSuspense.tsx

--- src/hoc/withSuspense.tsx
+++ src/hoc/withSuspense.tsx
@@ -4,7 +4,7 @@ import Preloader from '../components/Common/Preloader/Preloader'
 
 export const withSuspense = (Component: any) => {
     return (props: any) => {
-        return <React.Suspense fallback={<div>Loading...</div>}>
+        return <React.Suspense fallback={<Preloader/>}>
             <Component {...props} />
         </React.Suspense>
 
